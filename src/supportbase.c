28054b225d8efcb471cc45b93ca5df71d96b04cd
高峰 <362053534@qq.com>
2025-03-28 12:50:30 +0800

    cnlist test

--- src/supportbase.c
+++ src/supportbase.c
@@ -61,7 +61,7 @@ int sbCreateSemaphore(void)
 int isValidIsoName(char *name, int *pNameLen)
 {
     // Old ISO image naming format: SCUS_XXX.XX.ABCDEFGHIJKLMNOP.iso
-    strcpy(name , "SLUS_217.76.中文名.iso");
+
     // Minimum is 17 char, GameID (11) + "." (1) + filename (1 min.) + ".iso" (4)
     int size = strlen(name);
     if (strcasecmp(&name[size - 4], ".iso") == 0 || strcasecmp(&name[size - 4], ".zso") == 0) {
@@ -301,7 +301,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
         while ((dirent = readdir(dir)) != NULL) {
             int NameLen;
             int format = isValidIsoName(dirent->d_name, &NameLen);
-            dirent->d_name = path;
+            strcpy(dirent->d_name, path);
             if (format <= 0 || NameLen > ISO_GAME_NAME_MAX)
                 continue; // Skip files that cannot be supported properly.
 
