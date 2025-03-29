e8242565d7b4d157b458a7ccce586d4738bd133a
高峰 <362053534@qq.com>
2025-03-29 20:55:37 +0800

    Update supportbase.c

--- src/supportbase.c
+++ src/supportbase.c
@@ -111,7 +111,7 @@ int isValidIsoName(char *name, int *pNameLen)
     if (strcasecmp(&name[size - 4], ".iso") == 0 || strcasecmp(&name[size - 4], ".zso") == 0) {
         if ((size >= 17) && (name[4] == '_') && (name[8] == '.') && (name[11] == '.')) {
             //unicodeToUtf8(name[12], name);
-            asciiToUtf16(name, &name[12]);
+            asciiToUtf16(&name[12], &name[12]);
             ////setlocale(LC_ALL, "");                 // 设置当前区域为环境变量指定的区域
             //len = mbstowcs(wname, name, PATH_MAX); // 将多字节字符串转换为宽字符字符串
             ////   计算转换后的多字节字符串长度
