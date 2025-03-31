37dcf50de6545b199d066d78f204c96f609453a2
高峰 <362053534@qq.com>
2025-03-31 19:58:56 +0800

    Update supportbase.c

--- src/supportbase.c
+++ src/supportbase.c
@@ -560,15 +560,20 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                     char oldpath[256], newpath[256];
                     strcpy(oldpath, fullpath);
                     snprintf(newpath, strlen(fullpath), "%s%s", fullpath - strlen(dirent->d_name), "1.iso");
-                    //rename(fullpath, newpath);
+                    rename(fullpath, newpath);
                     // need to mount and read SYSTEM.CNF
                     int MountFD = fileXioMount("iso:", fullpath, FIO_MT_RDONLY);
                     if (MountFD < 0 || GetStartupExecName("iso:/SYSTEM.CNF;1", startup, GAME_STARTUP_MAX - 1) != 0) {
-                        fileXioUmount("iso:");
-                        //rename(newpath, oldpath);
-                        free(next);
-                        *glist = next->next;
-                        continue;
+                        fullpath[base_path_len] = '\\';
+                        if (MountFD < 0 || GetStartupExecName("iso:/SYSTEM.CNF;1", startup, GAME_STARTUP_MAX - 1) != 0) {
+                            fileXioUmount("iso:");
+                            // rename(newpath, oldpath);
+                            free(next);
+                            *glist = next->next;
+                            fullpath[base_path_len] = '/';
+                            continue;
+                        }
+                        fullpath[base_path_len] = '/';
                     }
                     memcpy(game->startup, startup, GAME_STARTUP_MAX - 1);
                     game->startup[GAME_STARTUP_MAX - 1] = '\0';
