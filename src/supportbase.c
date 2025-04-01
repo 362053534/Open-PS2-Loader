c6c7703f89ed5211bc73f05b18f77e653639cbea
高峰 <362053534@qq.com>
2025-04-01 14:51:31 +0800

    Update supportbase.c

--- src/supportbase.c
+++ src/supportbase.c
@@ -563,7 +563,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                     wchar_t *w_newpath = (wchar_t *)malloc(len * sizeof(wchar_t));
                     mbstowcs(w_fullpath, fullpath, len); // 将多字节字符串转换为宽字符字符串
                     mbstowcs(w_newpath, newpath, len);   // 将多字节字符串转换为宽字符字符串
-                    _wrename(w_fullpath, w_newpath);
+                    //_wrename(w_fullpath, w_newpath);
                     //rename(fullpath, newpath);
                     
                     // need to mount and read SYSTEM.CNF
@@ -573,7 +573,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                         oldpath[base_path_len] = fullpath[0] == 's' ? '\\' : '/';
                         sprintf(newpath, "%s%s%s", oldpath, "ggg", &dirent->d_name[NameLen]);
                         mbstowcs(w_newpath, newpath, len);   // 将多字节字符串转换为宽字符字符串
-                        _wrename(w_newpath,w_fullpath);
+                        //_wrename(w_newpath,w_fullpath);
                         //rename(newpath, fullpath);
                         free(next);
                         *glist = next->next;                                              
@@ -584,7 +584,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                     oldpath[base_path_len] = fullpath[0] == 's' ? '\\' : '/';
                     sprintf(newpath, "%s%s%s", oldpath, "ggg", &dirent->d_name[NameLen]);                                  
                     mbstowcs(w_newpath, newpath, len); // 将多字节字符串转换为宽字符字符串
-                    _wrename(w_newpath, w_fullpath);
+                    //_wrename(w_newpath, w_fullpath);
                     //rename(newpath, fullpath);
                     //support->itemRename(support, selected_item->item->current->item.id, fullpath);
                     //ioPutRequest(IO_MENU_UPDATE_DEFFERED, &support->mode);
