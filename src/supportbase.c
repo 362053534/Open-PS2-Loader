aee2caff49d5568cfcf8c364ec32982607afa20e
高峰 <362053534@qq.com>
2025-04-01 22:19:54 +0800

    crc test 4

--- src/supportbase.c
+++ src/supportbase.c
@@ -710,9 +710,12 @@ int sbReadList(base_game_info_t **list, const char *prefix, int *fsize, int *gam
                     snprintf(path, sizeof(path), "%s", prefix);
                     d = opendir(path); // 打开当前目录
                     if (d) {
+                        char str1[12];
+                        strncpy(str1, &dir->d_name[12], 11);
+                        str1[11] = '\0';
                         while ((dir = readdir(d)) != NULL) {
-                            if (strncmp(&dir->d_name[12], &g->startup[0], 11) {
-                                memcpy(g->crc32name, dir->d_name, 8);
+                            if (strncmp(str1, g->startup, 12) {
+                                memcpy(g->crc32name, &dir->d_name[3], 8);
                                 g->crc32name[8] = '\0';
                                 break;
                             }
