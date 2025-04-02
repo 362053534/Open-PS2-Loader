f3f5973c52d57a289b5ea95f9e071bdf2780f323
高峰 <362053534@qq.com>
2025-04-02 20:01:51 +0800

    Update supportbase.c

--- src/supportbase.c
+++ src/supportbase.c
@@ -515,10 +515,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
         fullpath[base_path_len] = '/';
 
         snprintf(path, 256, "%s%siso.txt", path, path[0] == 's' ? "\\" : "/");
-        char index[100];
-        memcpy(index, dirent->d_name, sizeof(index));
-        index[strlen(dirent->d_name) - 4] = '\0';
-        char cnName[256];
+
         file = fopen(path, "r");
 
 
@@ -640,6 +637,10 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
             }
 
             // count and process games in iso.txt
+            char index[100];
+            memcpy(index, dirent->d_name, sizeof(index));
+            index[strlen(dirent->d_name) - 4] = '\0';
+            char cnName[256];
             if (file != NULL) {
                 //strncpy(game->name, "打开文件了", 30);
                 while (fgets(cnName, sizeof(cnName), file) != NULL) {
@@ -649,7 +650,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                         break;
                     }
                 }
-                rewind(file);
+                //rewind(file);
             }
 
             //strncpy(game->name, path,30);
