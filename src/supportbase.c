4d23a547d4c2e8249765e8efc3595e731050eecf
高峰 <362053534@qq.com>
2025-04-02 19:37:51 +0800

    Update supportbase.c

--- src/supportbase.c
+++ src/supportbase.c
@@ -645,7 +645,6 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                         memcpy(game->name, &cnName[strlen(index) + 1], UL_GAME_NAME_MAX);
                         strncpy(game->name, "找到了名字", 30);
                         game->name[UL_GAME_NAME_MAX] = '\0';
-                        rewind(file);
                         break;
                     }
                 }
