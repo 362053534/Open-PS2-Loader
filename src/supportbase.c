7d05415c944ce24c3c0f686511e787925c1b068c
高峰 <362053534@qq.com>
2025-04-02 22:46:14 +0800

    Update supportbase.c

--- src/supportbase.c
+++ src/supportbase.c
@@ -1068,10 +1068,10 @@ void sbRebuildULCfg(base_game_info_t **list, const char *prefix, int gamecount, 
 
 static void sbCreatePath_name(const base_game_info_t *game, char *path, const char *prefix, const char *sep, int part, const char *game_name)
 {
-    if (game_name[0] >= '0' && game_name[0] <= '9') {
-        memcpy(game_name, game->nameIndex, strlen(game->nameIndex));
-        game_name[strlen(game->nameIndex)] = '\0';
-    }
+    //if (game_name[0] >= '0' && game_name[0] <= '9') {
+    //    memcpy(game_name, game->nameIndex, strlen(game->nameIndex));
+    //    game_name[strlen(game->nameIndex)] = '\0';
+    //}
 
     switch (game->format) {
         case GAME_FORMAT_USBLD:
