fd6f83ffd3aa28c5f7cde814d48d76285357d020
高峰 <362053534@qq.com>
2025-04-02 16:02:28 +0800

    Update supportbase.c

--- src/supportbase.c
+++ src/supportbase.c
@@ -639,14 +639,14 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
             if (file != NULL) {
                 while (fgets(cnName, sizeof(cnName), file) != NULL) {
                     if (strncmp(index, cnName, strlen(index) == 0)) {
-                        memcpy(game->name, &cnName[strlen(index) + 1], UL_GAME_NAME_MAX);
+                        strcpy(game->name, "成功了吗", UL_GAME_NAME_MAX);
                         game->name[UL_GAME_NAME_MAX] = '\0';
                         break;
                     }
                 }
             }
             fclose(file);
-
+                                    strcpy(game->name, "成功了吗", UL_GAME_NAME_MAX);
 
             game->parts = 1;
             game->media = type;
