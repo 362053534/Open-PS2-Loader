cb85e54f91dd377814077255466fba42fb63625e
高峰 <362053534@qq.com>
2025-04-02 16:30:47 +0800

    Update supportbase.c

--- src/supportbase.c
+++ src/supportbase.c
@@ -629,7 +629,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
             }
 
             // count and process games in iso.txt
-            snprintf(path, 256, "%s%siso.txt", path, path[0] == 's' ? '\\' : '/');
+            snprintf(path, 256, "%s%siso.txt", path, path[0] == 's' ? "\\" : "/");
             char index[100];
             memcpy(index,dirent->d_name,sizeof(index));
             index[strlen(dirent->d_name) - 4] = '\0';
