d6a661f0c23b36e26e4610eab972e3e39f7572b2
高峰 <362053534@qq.com>
2025-04-03 21:42:23 +0800

    Update supportbase.c

--- src/supportbase.c
+++ src/supportbase.c
@@ -683,7 +683,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                 if (file != NULL) {
                     rewind(file);
                     if (fgets(fullName, sizeof(fullName), file) != NULL) {
-                        if (fullname[0] == '\0')
+                        if (fullName[0] == '\0')
                             fprintf(file, "// “.”符号左侧为iso英文名，右侧写上对应的翻译文本，即可实现游戏列表中文化！\n// 每一行对应一个游戏，最后一个游戏名一定要加上回车换行！\n\n");
                         }
                     }
