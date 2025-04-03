d21b05a77d2328f9e5e24e214aa7d9000507088b
高峰 <362053534@qq.com>
2025-04-03 14:10:17 +0800

    Update supportbase.c

--- src/supportbase.c
+++ src/supportbase.c
@@ -688,6 +688,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                             strncpy(game->name, &cnName[strlen(index) + 1], UL_GAME_NAME_MAX);
                             memcpy(game->nameIndex, index, strlen(index));
                             game->nameIndex[strlen(index)] = '\0';
+                            sprintf(game->name, "%d", game->name);
                             for (int i = 0; i < strlen(cnName); i++) {
                                 if (cnName[i] == '\n' || cnName[i] == '\0' || cnName[i] == '\r' || &cnName[i] == "") {
                                     game->name[i - strlen(index) - 1] = '\0';
@@ -714,6 +715,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                             strncpy(game->name, &cnName[strlen(index) + 1], UL_GAME_NAME_MAX);
                             memcpy(game->nameIndex, index, strlen(index));
                             game->nameIndex[strlen(index)] = '\0';
+                            sprintf(game->name, "%d", game->name);
                             for (int i = 0; i < strlen(cnName); i++) {
                                 if (cnName[i] == '\n' || cnName[i] == '\0' || cnName[i] == '\r' || &cnName[i] == "") {
                                     game->name[i - strlen(index) - 1] = '\0';
