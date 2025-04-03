aff865abd361e9a8c0d0f9a77ef61ca0984d7b22
高峰 <362053534@qq.com>
2025-04-03 22:17:08 +0800

    Update supportbase.c

--- src/supportbase.c
+++ src/supportbase.c
@@ -518,7 +518,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
         char fullName[64];
         snprintf(path, 256, "%s%s../Title Translator.txt", path, path[0] == 's' ? "\\" : "/");
         file = fopen(path, "a+");
-
+        rewind(file);
 
 
         while ((dirent = readdir(dir)) != NULL) {
@@ -680,7 +680,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                 memcpy(_indexName, &dirent->d_name[GAME_STARTUP_MAX], sizeof(_indexName));
                 _indexName[strlen(dirent->d_name) - 4 - GAME_STARTUP_MAX] = '\0';     // 临时的索引名
                 //memcpy(game->nameIndex, _indexName, strlen(_indexName)); // 存在，就赋值给索引数组
-                if (file != NULL) {              
+                if (file != NULL) {
                     if (fgets(fullName, sizeof(fullName), file) == NULL) {
                         fprintf(file, "// “.”符号左侧为iso英文名，右侧写上对应的翻译文本，即可实现游戏列表中文化！\n// 每一行对应一个游戏，最后一个游戏名一定要加上回车换行！\n\n");       
                     }
