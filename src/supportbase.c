6f07da393f47e653fadfbed23e50f95afe9bb4ae
高峰 <362053534@qq.com>
2025-04-04 20:48:11 +0800

    debug fullname int

--- src/supportbase.c
+++ src/supportbase.c
@@ -707,6 +707,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                             break;
                         }
                     }
+                    fprintf(file, "%d.\n", fullName);
                     if (fullName == NULL && game->transName[0] == '\0') {
                         //rewind(file);
                         fprintf(file, "%s.\n" ,game->nameIndex);
@@ -742,6 +743,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                             break;
                         }
                     }
+                    fprintf(file, "%d.\n", fullName);
                     if (fullName == NULL && game->transName[0] == '\0') {
                         // rewind(file);
                         fprintf(file, "%s.\n", game->nameIndex);
