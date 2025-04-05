300f391a0ad969813978649a14a6851a02254752
高峰 <362053534@qq.com>
2025-04-05 16:34:28 +0800

    测试\r\n

--- src/supportbase.c
+++ src/supportbase.c
@@ -692,7 +692,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                             //memcpy(game->name, nameIndex, strlen(nameIndex));  
                             //game->name[strlen(nameIndex)] = '\0';
                             strcpy(game->nameIndex, game->name);     // 存在，就赋值给索引数组                                                                                     // 将真正的游戏名变成index索引名
-                            if (fullName[strlen(game->nameIndex) + 1] == '\n' || fullName[strlen(game->nameIndex) + 1] == '\0') { // 判断索引的译名是否为空
+                            if (fullName[strlen(game->nameIndex) + 1] == '\n' || fullName[strlen(game->nameIndex) + 1] == '\0' || fullName[strlen(game->nameIndex) + 1] == '\r') { // 判断索引的译名是否为空
                                 game->transName[0] = '\0';
                                 break;
                             }
@@ -713,7 +713,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                     // 如果txt里没有此游戏的英文名索引，则添加到txt里
                     if (game->nameIndex[0] == '\0' && game->transName[0] == '\0') {
                         strcpy(game->nameIndex, game->name); // 将真正的游戏名变成index索引名
-                        fprintf(file, "%s.\n", game->nameIndex);
+                        fprintf(file, "%s.\0\r\n", game->nameIndex);
                         // strncpy(game->nameIndex, nameIndex, strlen(nameIndex)); // 不存在就添加一笔，然后赋值给索引数组
                     }
                 }
