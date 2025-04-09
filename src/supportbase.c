c36abc66f16db0f2c0fa10370fe15462d7113976
高峰 <362053534@qq.com>
2025-04-09 14:32:23 +0800

    继续测试字符边界

--- src/supportbase.c
+++ src/supportbase.c
@@ -701,7 +701,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                          
                             //sprintf(game->name, "%d", game->name[0]);
                             //给游戏名加结束符，防止换行符被显示出来
-                            game->name[strlen(game->name)] = '\0';
+                            game->name[strlen(game->name) - 1] = '\0';
                             //for (int i = 0; i < strlen(fullName); i++) {
                             //    if (fullName[i] == '\r' || fullName[i] == '\n' || fullName[i] == '\0') {
                             //        game->name[i - strlen(game->nameIndex) - 1] = '\0';
