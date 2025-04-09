5e76181d8fb72672728dcde5262e2e44eecb1630
高峰 <362053534@qq.com>
2025-04-09 14:41:30 +0800

    更换译名添加\0的算法

--- src/supportbase.c
+++ src/supportbase.c
@@ -701,7 +701,12 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                          
                             //sprintf(game->name, "%d", game->name[0]);
                             //给游戏名加结束符，防止换行符被显示出来
-                            game->name[strlen(game->name) - 1] = '\0';
+                            for (int i = 0; i < strlen(game->transName); i++) {
+                                if (game->transName[i] == '\r' || game->transName[i] == '\n' || game->transName[i] == '\0') {
+                                    game->name[i] = '\0';
+                                    break;
+                                }
+                            }
                             //for (int i = 0; i < strlen(fullName); i++) {
                             //    if (fullName[i] == '\r' || fullName[i] == '\n' || fullName[i] == '\0') {
                             //        game->name[i - strlen(game->nameIndex) - 1] = '\0';
