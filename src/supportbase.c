50af78ccdc07ab319d6964dbe650dbcc4f923ff1
高峰 <362053534@qq.com>
2025-04-12 00:24:26 +0800

    Update supportbase.c

--- src/supportbase.c
+++ src/supportbase.c
@@ -526,7 +526,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
         }
         // 使用stat函数获取文件修改时间
         struct stat fileStat;
-        if (stat(txtPath, &fileStat) == 0) {
+        if (fstat(file, &fileStat) == 0) {
             time_t curTxtModiTime = fileStat.st_mtime;
         }
 
