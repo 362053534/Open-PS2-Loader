316fa1f4ac124461fa9333067a51e54a72c32328
高峰 <362053534@qq.com>
2025-04-12 02:17:14 +0800

    Update supportbase.c

--- src/supportbase.c
+++ src/supportbase.c
@@ -795,10 +795,10 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
         char debugFileDir[64];
         snprintf(debugFileDir, 256, "%s%cdebug.txt", path, path[0] == 's' ? '\\' : '/');
         FILE *debugFile = fopen(debugFileDir, "ab");
-        fprintf(debugFile, "%s.缓存时间戳%d.文件时间戳%d\r\n", skipTxtScan ? "跳过了txt扫描" : "进行了txt扫描", cache.games[0]->preModiTime, fileStat.st_mtime);
+        fprintf(debugFile, "%s.缓存时间戳%d.文件时间戳%d\r\n", skipTxtScan ? "跳过了txt扫描" : "进行了txt扫描", cache.games[0].preModiTime, fileStat.st_mtime);
         // 使用stat函数获取保存后的txt修改时间
         if (stat(txtPath, &fileStat) == 0) {
-            if (cache.games[0]) {
+            if (cache.games[0] != NULL) {
                 cache.games[0].preModiTime = fileStat.st_mtime; // txt操作完毕后，将它保存在缓存里。
             }
         }
