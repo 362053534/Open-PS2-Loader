feaa7358dc88720414e06fd38f72b2c4dea82f4a
高峰 <362053534@qq.com>
2025-04-12 01:13:28 +0800

    Update supportbase.c

--- src/supportbase.c
+++ src/supportbase.c
@@ -578,10 +578,14 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                 // use cached entry
                 memcpy(game, &cachedGInfo, sizeof(base_game_info_t));
 
-                // 通过文件修改时间判断文本是否改动，未改动则跳过txt扫描，提升效率
-                if (strncpy(curTxtModiTime, cache.txtModiTime, sizeof(time_t)) == 0) {
-                        skipTxtScan = 1;
-                }         
+                time_t a = 0, b = 0;
+                if (a==b) {
+                    a = b;
+                }
+                //// 通过文件修改时间判断文本是否改动，未改动则跳过txt扫描，提升效率
+                //if (strncpy(curTxtModiTime, cache.txtModiTime, sizeof(time_t)) == 0) {
+                //        skipTxtScan = 1;
+                //}         
             } else {
                 // if (true)
 
