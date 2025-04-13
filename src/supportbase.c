a784e69b248fa091d144d7c211fd88976c122686
高峰 <362053534@qq.com>
2025-04-13 18:57:09 +0800

    关闭debug相关代码，进行测试

--- src/supportbase.c
+++ src/supportbase.c
@@ -489,10 +489,10 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
     struct dirent *dirent;
     DIR *dir;
 
-    // debug 文件
-    char debugFileDir[64];
-    snprintf(debugFileDir, 256, "%s%cdebug.txt", path, path[0] == 's' ? '\\' : '/');
-    FILE *debugFile = fopen(debugFileDir, "ab");
+    //// debug 文件
+    //char debugFileDir[64];
+    //snprintf(debugFileDir, 256, "%s%cdebug.txt", path, path[0] == 's' ? '\\' : '/');
+    //FILE *debugFile = fopen(debugFileDir, "ab");
 
     int cacheLoaded = loadISOGameListCache(path, &cache) == 0;
     int skipTxtScan = 0;
@@ -566,7 +566,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                 sprintf(fileName, "%s%s", game->name, game->extension);
                 if (cacheLoaded && queryISOGameListCache(&cache, &cachedGInfo, fileName) == 0) {
                     // 如果缓存中已有索引条目，且txt未更新，则跳过txt扫描，加快游戏列表生成速度
-                    fprintf(debugFile, "old查到缓存；文件名：%s；索引名：%s\r\n", fileName, cachedGInfo.indexName);
+                    //fprintf(debugFile, "old查到缓存；文件名：%s；索引名：%s\r\n", fileName, cachedGInfo.indexName);
                     if (cachedGInfo.indexName[0] != '\0' && !txtFileChanged) {
                         skipTxtScan = 1;
                         strcpy(game->indexName, cachedGInfo.indexName);
@@ -587,7 +587,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
             } else if (cacheLoaded && queryISOGameListCache(&cache, &cachedGInfo, dirent->d_name) == 0) {
                 // use cached entry
                 memcpy(game, &cachedGInfo, sizeof(base_game_info_t));
-                fprintf(debugFile, "new查到缓存；文件名：%s；索引名：%s\r\n", dirent->d_name, game->indexName);
+                //fprintf(debugFile, "new查到缓存；文件名：%s；索引名：%s\r\n", dirent->d_name, game->indexName);
                 // 如果缓存中已有索引条目，且txt未更新，则跳过txt扫描，加快游戏列表生成速度
                 if (cachedGInfo.indexName[0] != '\0' && !txtFileChanged) {
                     skipTxtScan = 1;
@@ -805,14 +805,14 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
             //else
             //    snprintf(path, 256, "%s%s%s%s%s", prefix, (game->media == SCECdPS2CD) ? "CD" : "DVD", sep, game_name, game->extension);
             //strncpy(game->name, path, 40);
-                fprintf(debugFile, "有没有跳过txt扫描：%s：%d\r\n", game->name,skipTxtScan);
+                //fprintf(debugFile, "有没有跳过txt扫描：%s：%d\r\n", game->name,skipTxtScan);
                 count++;
         }
         fclose(file);
 
         // debug 确认txt跳过扫描是否生效
-        fprintf(debugFile, "%s.缓存时间戳%d.文件时间戳%d.缓存的第一个游戏名%s.glist第一个游戏名%s\r\n", skipTxtScan ? "跳过了txt扫描" : "进行了txt扫描", cache.games[0].preModiTime, fileStat.st_mtime, cache.games[0].name, glist[0]->gameinfo.name);
-        fclose(debugFile);
+        //fprintf(debugFile, "%s.缓存时间戳%d.文件时间戳%d.缓存的第一个游戏名%s.glist第一个游戏名%s\r\n", skipTxtScan ? "跳过了txt扫描" : "进行了txt扫描", cache.games[0].preModiTime, fileStat.st_mtime, cache.games[0].name, glist[0]->gameinfo.name);
+        //fclose(debugFile);
 
         // 使用stat函数获取保存后的txt修改时间
         if (stat(txtPath, &fileStat) == 0) {
