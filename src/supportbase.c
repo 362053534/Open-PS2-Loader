db9d17d5d39423b354c50fee4f75dd9088c22017
高峰 <362053534@qq.com>
2025-04-15 22:07:48 +0800

    屏蔽cache.games操作

--- src/supportbase.c
+++ src/supportbase.c
@@ -519,7 +519,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
     char curModiTime[6];
     char preModiTime[6];
     iox_stat_t fileStat;
-    strncpy(preModiTime, cache.games[0].preModiTime, 6);
+    //strncpy(preModiTime, cache.games[0].preModiTime, 6);
     if (fileXioGetStat(txtPath, &fileStat) >= 0) {
         // 通过文件修改时间判断txt是否改动
         sprintf(curModiTime, "%02u%02u%02u", fileStat.mtime[1], fileStat.mtime[2], fileStat.mtime[3]);
