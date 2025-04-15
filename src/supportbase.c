4d5dfd4afa218ee0bdc3a636945baf21daa0ed97
高峰 <362053534@qq.com>
2025-04-16 01:23:25 +0800

    Update supportbase.c

--- src/supportbase.c
+++ src/supportbase.c
@@ -552,8 +552,13 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
 
     //memcpy(preModiTime, cache.games[0].preModiTime, sizeof(preModiTime));
     //sprintf(preModiTime, "%s", &cache->games[0].preModiTime);
-    sprintf(preModiTime, "%s", cache.games[0].preModiTime);
-    preModiTime[6] = '\0';
+    if (cache.count > 0) {
+        sprintf(preModiTime, "%s", cache.games[0].preModiTime);
+        preModiTime[6] = '\0';
+    } else {
+        sprintf(preModiTime, "000000");
+        preModiTime[6] = '\0';
+    }
     //loadCacheMtime(&cache, preModiTime);
 
     if (fileXioGetStat(txtPath, &fileStat) >= 0) {
