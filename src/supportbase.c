fa1202d8a86ea4ef5deb5ce71cbbd879d74ae955
高峰 <362053534@qq.com>
2025-04-16 16:13:17 +0800

    关闭debug，修改closefile的位置

--- src/supportbase.c
+++ src/supportbase.c
@@ -521,10 +521,10 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
     struct dirent *dirent;
     DIR *dir;
 
-    // debug 文件
-    char debugFileDir[64];
-    snprintf(debugFileDir, 256, "%s%cdebug.txt", path, path[0] == 's' ? '\\' : '/');
-    FILE *debugFile = fopen(debugFileDir, "ab");
+    //// debug 文件
+    //char debugFileDir[64];
+    //snprintf(debugFileDir, 256, "%s%cdebug.txt", path, path[0] == 's' ? '\\' : '/');
+    //FILE *debugFile = fopen(debugFileDir, "ab");
 
     int cacheLoaded = loadISOGameListCache(path, &cache) == 0;
     int skipTxtScan = 0;
@@ -578,8 +578,8 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
         //sprintf(curModiTime, "000000");
         //curModiTime[6] = '\0';
     }
-    // debug
-    fprintf(debugFile, "文件时间%s和缓存时间%s\r\n", curModiTime, preModiTime);
+    //// debug
+    //fprintf(debugFile, "文件时间%s和缓存时间%s\r\n", curModiTime, preModiTime);
 
     // 使用stat函数获取文件修改时间，与缓存进行比对
     // struct stat fileStat;
@@ -642,8 +642,8 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                 if (cacheLoaded && queryISOGameListCache(&cache, &cachedGInfo, fileName) == 0) {
                     // 如果缓存中已有索引条目，且txt未更新，则跳过txt扫描，加快游戏列表生成速度
                     
-                    // debug
-                    fprintf(debugFile, "old查到缓存；文件名：%s；索引名：%s\r\n", fileName, (&cachedGInfo)->indexName);
+                    //// debug
+                    //fprintf(debugFile, "old查到缓存；文件名：%s；索引名：%s\r\n", fileName, (&cachedGInfo)->indexName);
 
                     if ((&cachedGInfo)->indexName[0] != '\0' && !txtFileChanged) {
                         skipTxtScan = 1;
@@ -671,8 +671,8 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
                     strcpy(game->name, game->indexName);
                 }
 
-                // debug
-                fprintf(debugFile, "new查到缓存；文件名：%s；索引名：%s\r\n", dirent->d_name, game->indexName);
+                //// debug
+                //fprintf(debugFile, "new查到缓存；文件名：%s；索引名：%s\r\n", dirent->d_name, game->indexName);
 
                 // 如果缓存中已有索引条目，且txt未更新，则跳过txt扫描，加快游戏列表生成速度
                 if ((&cachedGInfo)->indexName[0] != '\0' && !txtFileChanged) {
@@ -889,13 +889,13 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
             //    snprintf(path, 256, "%s%s%s%s%s", prefix, (game->media == SCECdPS2CD) ? "CD" : "DVD", sep, game_name, game->extension);
             //strncpy(game->name, path, 40);
 
-                // debug
-                fprintf(debugFile, "有没有跳过txt扫描：%s：%d\r\n", game->name, skipTxtScan);
+                //// debug
+                //fprintf(debugFile, "有没有跳过txt扫描：%s：%d\r\n", game->name, skipTxtScan);
 
                 count++;
         }
         //free(tempIndexName);
-        fclose(file);
+
 
         // 使用newlib的stat函数获取文件修改时间，与缓存进行比对
         if (fileXioGetStat(txtPath, &fileStat) >= 0) {
@@ -917,10 +917,10 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
             //memcpy(glist[0]->gameinfo.preModiTime, curModiTime, sizeof(curModiTime)); // txt操作完毕后，将它保存在glist里。
         }
 
-        // debug 确认txt跳过扫描是否生效
-        fprintf(debugFile, "文件载入时间戳%s，文件关闭时间戳%s，缓存的第一个游戏名%s，glist第一个游戏名%s\r\n", preModiTime, curModiTime, (&cache)->games[0].name, (*glist)->gameinfo.name);
-        fprintf(debugFile, "路径：%s\r\n\r\n", txtPath);
-        fclose(debugFile);
+        //// debug 确认txt跳过扫描是否生效
+        //fprintf(debugFile, "文件载入时间戳%s，文件关闭时间戳%s，缓存的第一个游戏名%s，glist第一个游戏名%s\r\n", preModiTime, curModiTime, (&cache)->games[0].name, (*glist)->gameinfo.name);
+        //fprintf(debugFile, "路径：%s\r\n\r\n", txtPath);
+        //fclose(debugFile);
 
         //// 使用stat函数获取保存后的txt修改时间
         //if (stat(txtPath, &fileStat) == 0) {
@@ -931,7 +931,7 @@ static int scanForISO(char *path, char type, struct game_list_t **glist)
         // 
         //    //*glist.gameinfo.preModiTime;
         //}
-
+        fclose(file);
         closedir(dir);
     }
 
