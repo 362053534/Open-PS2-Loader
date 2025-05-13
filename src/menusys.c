0a47fc899da63841b8b44bc5ab0dd21e9fe4f209
高峰 <362053534@qq.com>
2025-05-14 02:12:36 +0800

    test

--- src/menusys.c
+++ src/menusys.c
@@ -924,8 +924,8 @@ void menuHandleInputMenu()
     if (getKeyOn(KEY_START) || getKeyOn(gSelectButton == KEY_CIRCLE ? KEY_CROSS : KEY_CIRCLE)) {
         // Check if there is anything to show the user, at all.
         if (gAPPStartMode || gETHStartMode || gBDMStartMode || gHDDStartMode) {
-            guiSwitchScreen(GUI_SCREEN_MAIN);
             refreshMenuPosition();
+            guiSwitchScreen(GUI_SCREEN_MAIN);
         }
     }
 }
