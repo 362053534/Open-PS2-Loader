2423fcf7e87ed2b1bf22bc4ec73352a850af920b
高峰 <362053534@qq.com>
2025-05-13 15:35:32 +0800

    test2

--- src/opl.c
+++ src/opl.c
@@ -420,7 +420,7 @@ void initSupport(item_list_t *itemList, int mode, int force_reinit)
 
     if (startMode) {
         // usb关闭时，不显示U盘游戏列表
-        if ((mode == 0) && !gEnableUSB) {
+        if ((mode == BDM_MODE1) && !gEnableUSB) {
             int dir = fileXioDopen("mass0:/");
             char bdmDriver[32];
             if (dir >= 0) {
