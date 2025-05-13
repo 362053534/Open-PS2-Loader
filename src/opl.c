d6f185fa06d66fbaeab766fb23711344211591c3
高峰 <362053534@qq.com>
2025-05-13 16:13:13 +0800

    test

--- src/opl.c
+++ src/opl.c
@@ -344,7 +344,7 @@ static void initMenuForListSupport(opl_io_module_t *mod)
     mod->menuItem.icon_id = mod->support->itemIconId(mod->support);
     mod->menuItem.text = NULL;
     mod->menuItem.text_id = mod->support->itemTextId(mod->support);
-    mod->menuItem.visible = 0;
+    mod->menuItem.visible = 1;
 
     mod->menuItem.userdata = mod->support;
 
@@ -438,9 +438,29 @@ void initSupport(item_list_t *itemList, int mode, int force_reinit)
             mod->support = itemList;
             mod->support->owner = mod;
             initMenuForListSupport(mod);
+
+            // usb关闭时，不显示U盘游戏列表
+            if ((mode == 0) && !gEnableUSB) {
+                int dir = fileXioDopen("mass0:/");
+                char bdmDriver[32];
+                if (dir >= 0) {
+                    fileXioIoctl2(dir, USBMASS_IOCTL_GET_DRIVERNAME, NULL, 0, bdmDriver, sizeof(bdmDriver) - 1);
+                    if (!strcmp(bdmDriver, "usb")) {
+                        mod->menuItem.visible = 0;
+                        fileXioDclose(dir);
+                        return;
+                    }
+                    fileXioDclose(dir);
+                }
+            }
         }
 
         if (((force_reinit) && (mod->support->enabled)) || (startMode == START_MODE_AUTO && !mod->support->enabled)) {
+            mod->support->itemInit(mod->support);
+            moduleUpdateMenuInternal(mod, 0, 0);
+
+            ioPutRequest(IO_MENU_UPDATE_DEFFERED, &list_support[mode].support->mode); // can't use mode as the variable will die at end of execution
+
             // usb关闭时，不显示U盘游戏列表
             if ((mode == 0) && !gEnableUSB) {
                 int dir = fileXioDopen("mass0:/");
@@ -453,11 +473,6 @@ void initSupport(item_list_t *itemList, int mode, int force_reinit)
                     fileXioDclose(dir);
                 }
             }
-
-            mod->support->itemInit(mod->support);
-            moduleUpdateMenuInternal(mod, 0, 0);
-
-            ioPutRequest(IO_MENU_UPDATE_DEFFERED, &list_support[mode].support->mode); // can't use mode as the variable will die at end of execution
         }
     } else {
         // If the module has a valid menu instance try to refresh the visibility state.
