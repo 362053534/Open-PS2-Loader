10ba2f16e87b986ef3593877a5c27414e2eec54f
高峰 <362053534@qq.com>
2025-05-13 14:17:34 +0800

    test

--- src/opl.c
+++ src/opl.c
@@ -425,21 +425,19 @@ void initSupport(item_list_t *itemList, int mode, int force_reinit)
             initMenuForListSupport(mod);
         }
 
-        // usb关闭时，不显示U盘游戏列表
-        if ((mode == 0) && !gEnableUSB) {
-            bdm_device_data_t *pDeviceData = itemList->priv;
-            if (pDeviceData->bdmDeviceType == BDM_TYPE_USB) {
-                mod->menuItem.visible = 0;
-                ((opl_io_module_t *)itemList->owner)->menuItem.visible = 0;
-                ((opl_io_module_t *)mod->support->owner)->menuItem.visible = 0;
-                return;
-            }
-        }
-
         if (((force_reinit) && (mod->support->enabled)) || (startMode == START_MODE_AUTO && !mod->support->enabled)) {
             mod->support->itemInit(mod->support);
             moduleUpdateMenuInternal(mod, 0, 0);
 
+            // usb关闭时，不显示U盘游戏列表
+            if ((mode == 0) && !gEnableUSB) {
+                bdm_device_data_t *pDeviceData = itemList->priv;
+                if (pDeviceData->bdmDeviceType == BDM_TYPE_USB) {
+                    mod->menuItem.visible = 0;
+                    ((opl_io_module_t *)itemList->owner)->menuItem.visible = 0;
+                    ((opl_io_module_t *)mod->support->owner)->menuItem.visible = 0;
+                }
+            }
             ioPutRequest(IO_MENU_UPDATE_DEFFERED, &list_support[mode].support->mode); // can't use mode as the variable will die at end of execution
         }
     } else {
