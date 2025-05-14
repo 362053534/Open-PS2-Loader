76e39039617ec9b0da7bf2d623c3849404cba428
高峰 <362053534@qq.com>
2025-05-14 16:08:18 +0800

    Update gui.h

--- include/gui.h
+++ include/gui.h
@@ -59,7 +59,7 @@ extern int guiFrameId;
 #define GUI_SCREEN_GAME_MENU 3
 #define GUI_SCREEN_APP_MENU  4
 
-void guiSwitchScreen(int target, int _transIndex);
+void guiSwitchScreen(int target, int _transIndex = 0);
 
 void guiReloadScreenExtents();
 
