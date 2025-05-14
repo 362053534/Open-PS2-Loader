de0c0f5eb0e45c97f42f6ed6cf19c0325247f43e
高峰 <362053534@qq.com>
2025-05-14 18:30:25 +0800

    test3

--- src/gui.c
+++ src/gui.c
@@ -1558,7 +1558,7 @@ void guiIntroLoop(void)
 void guiMainLoop(void)
 {
     int greetingAlpha = 0x80;
-    int endIntroDelayFrame = 60;
+    int endIntroDelayFrame = 120;
     int mainScreenSwitchDone = 0;
 
     guiResetNotifications();
@@ -1579,7 +1579,7 @@ void guiMainLoop(void)
         // 把intro界面淡出移到mainloop里，并添加一定延迟，保证淡出时，封面和游戏列表已加载完毕。
         if (endIntroDelayFrame > 0) {
             endIntroDelayFrame--;
-            if (endIntroDelayFrame <= 30) {
+            if (endIntroDelayFrame <= 100) {
                 if (!mainScreenSwitchDone) {
                     guiSwitchScreenFadeIn(GUI_SCREEN_MAIN, 13);
                     mainScreenSwitchDone = 1;
