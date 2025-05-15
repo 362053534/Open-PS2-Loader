ff10d3694b6729132ddaf8a6044cc1b14b08613b
高峰 <362053534@qq.com>
2025-05-15 14:44:35 +0800

    Update bdmsupport.c

--- src/bdmsupport.c
+++ src/bdmsupport.c
@@ -743,8 +743,7 @@ void bdmInitDevicesData()
                     pOwner->menuItem.visible = 0;
                     ((bdm_device_data_t *)bdmDeviceList[i].priv)->bdmDeviceTick = -1;
                 } else {
-                    //pOwner->menuItem.visible = (i == 0 ? 1 : 0);
-                    pOwner->menuItem.visible = 0;
+                    pOwner->menuItem.visible = (i == 0 ? 1 : 0);
                     ((bdm_device_data_t *)bdmDeviceList[i].priv)->bdmDeviceTick = -1;
                 }
             } else if (gBDMStartMode == START_MODE_AUTO) {
