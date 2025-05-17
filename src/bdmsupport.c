d5f553752dee29917604a16bc008ce4d5c6181ca
高峰 <362053534@qq.com>
2025-05-18 00:19:52 +0800

    test

--- src/bdmsupport.c
+++ src/bdmsupport.c
@@ -178,7 +178,7 @@ static int bdmNeedsUpdate(item_list_t *itemList)
         switch (pDeviceData->bdmDeviceType) {
             case BDM_TYPE_USB:
                 //deviceEnabled = (gBDMStartMode != START_MODE_DISABLED);
-                deviceEnabled = (gEnableUSB || gEnableILK || gEnableMX4SIO || gEnableBdmHDD);
+                deviceEnabled = gEnableUSB;
                 break;
             case BDM_TYPE_ILINK:
                 deviceEnabled = gEnableILK;
