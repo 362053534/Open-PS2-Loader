ff016ff3fb3b2daaaca47ad5ac2fb62d57b77898
峰哥 <362053534@qq.com>
2025-07-06 16:21:51 +0800

    test

--- src/fntsys.c
+++ src/fntsys.c
@@ -21,7 +21,7 @@ extern void *poeveticanew_raw;
 extern int size_poeveticanew_raw;
 
 /// Maximal count of atlases per font
-#define ATLAS_MAX    26
+#define ATLAS_MAX    28
 /// Atlas width in pixels
 #define ATLAS_WIDTH  128
 /// Atlas height in pixels
