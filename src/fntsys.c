1cc5f76873d08871c1cfeaf2093ee9ca5e67e54a
峰哥 <362053534@qq.com>
2025-07-06 13:13:07 +0800

    尝试解决高分辨率时，文字错乱问题

--- src/fntsys.c
+++ src/fntsys.c
@@ -21,7 +21,7 @@ extern void *poeveticanew_raw;
 extern int size_poeveticanew_raw;
 
 /// Maximal count of atlases per font
-#define ATLAS_MAX    6
+#define ATLAS_MAX    8
 /// Atlas width in pixels
 #define ATLAS_WIDTH  256
 /// Atlas height in pixels
