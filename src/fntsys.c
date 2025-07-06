4792123b616851392cc3ad8536e62b1200006fff
峰哥 <362053534@qq.com>
2025-07-07 00:53:00 +0800

    test

--- src/fntsys.c
+++ src/fntsys.c
@@ -27,7 +27,7 @@ extern int size_poeveticanew_raw;
 /// Atlas height in pixels
 #define ATLAS_HEIGHT 128
 
-#define GLYPH_CACHE_PAGE_SIZE 1 // 需要根据DPI进行动态变化
+#define GLYPH_CACHE_PAGE_SIZE 0 // 需要根据DPI进行动态变化
 
 // freetype vars
 static FT_Library font_library;
