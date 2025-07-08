d052c38abff2ba998d14af9a304ce210c7d72077
峰哥 <362053534@qq.com>
2025-07-09 04:20:25 +0800

    test

--- src/texcache.c
+++ src/texcache.c
@@ -246,6 +246,19 @@ GSTEXTURE *cacheGetTexture(image_cache_t *cache, item_list_t *list, int *cacheId
                 //} else if (!strncmp("BG", cache->suffix, 2)) {
                 //    PrevCacheID_BG = *cacheId;
                 //}
+                //  根据图像类型，赋值上一次的缓存
+                if (!strncmp("COV", cache->suffix, 3)) {
+                    if (PrevCacheID_COV >= 0)
+                        prevCache = &(&cache->content[PrevCacheID_COV])->texture;
+                } else if (!strncmp("ICO", cache->suffix, 3)) {
+                    if (PrevCacheID_ICO >= 0)
+                        prevCache = &(&cache->content[PrevCacheID_ICO])->texture;
+                } else if (!strncmp("BG", cache->suffix, 2)) {
+                    if (PrevCacheID_BG >= 0)
+                        prevCache = &(&cache->content[PrevCacheID_BG])->texture;
+                } else {
+                    prevCache = NULL;
+                }
                 return prevCache;
             } else {
                 entry->lastUsed = guiFrameId;
@@ -260,13 +273,13 @@ GSTEXTURE *cacheGetTexture(image_cache_t *cache, item_list_t *list, int *cacheId
                 // 根据图像类型，赋值上一次的缓存
                 if (!strncmp("COV", cache->suffix, 3)) {
                     if (PrevCacheID_COV >= 0)
-                        prevCache = &(&cache->content[PrevCacheID_COV])->texture;
+                        prevCache = &entry->texture;
                 } else if (!strncmp("ICO", cache->suffix, 3)) {
                     if (PrevCacheID_ICO >= 0)
-                        prevCache = &(&cache->content[PrevCacheID_ICO])->texture;
+                        prevCache = &entry->texture;
                 } else if (!strncmp("BG", cache->suffix, 2)) {
                     if (PrevCacheID_BG >= 0)
-                        prevCache = &(&cache->content[PrevCacheID_BG])->texture;
+                        prevCache = &entry->texture;
                 } /*else {
                     prevCache = NULL;
                 }*/
