f4fc6b136f89ad35ab3268e7d2a656922f6cf11f
峰哥 <362053534@qq.com>
2025-07-19 20:16:47 +0800

    debug

--- src/texcache.c
+++ src/texcache.c
@@ -281,11 +281,10 @@ GSTEXTURE *cacheGetTexture(image_cache_t *cache, item_list_t *list, int *cacheId
 
     // 触发加载CD
     if ((prevGuiFrameId != guiFrameId) && artCount) {
+        artLoadedCount = artCount;
         if (!buttonDelay) // CD不存在时，才会重置artCount，连续按按键时不重置。
             artCount = 0;
-        if (artCount)
-            artLoadedCount = artCount;
-        buttonDelay = 30;
+        buttonDelay = 100;
         prevGuiFrameId = guiFrameId;
     }
     //  触发CD，且已加载全部art时，跳过缓存ART
