3fac50f021207117a54d4f624f930f15d87db3a1
峰哥 <362053534@qq.com>
2025-08-02 22:32:19 +0800

    Update gui.c

--- src/gui.c
+++ src/gui.c
@@ -1607,7 +1607,7 @@ int endIntroDelayFrame = 0;
 int txtFileCreated = 0;
 int txtFileRebuilded = 0;
 int bdmTimeOut = 0;
-int artLoadDelayTime = 40;
+int artLoadDelayTime = 60;
 
 void reFindBDM()
 {
