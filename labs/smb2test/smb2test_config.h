0a3d2b0f51f0d4fe2005b2103a4c6862ec86ef74
峰哥 <362053534@qq.com>
2026-08-01 18:46:32 +0800

    Update smb2test_config.h

--- labs/smb2test/smb2test_config.h
+++ labs/smb2test/smb2test_config.h
@@ -5,8 +5,8 @@
 #define SMB2TEST_PS2_IP       "192.168.5.100"
 #define SMB2TEST_NETMASK      "255.255.255.0"
 #define SMB2TEST_GATEWAY      "192.168.5.4"
-/* PCAP 测局域网用 192.168.5.4；PCSX2 Socket 连本机共享请改成 127.0.0.1 */
-#define SMB2TEST_SERVER       "127.0.0.1"
+/* PCSX2「端口/Sockets」模式下，本机服务填网关地址（与拦截DHCP的网关一致），不要填 127.0.0.1 */
+#define SMB2TEST_SERVER       "192.168.5.4"
 #define SMB2TEST_PORT         445
 #define SMB2TEST_SHARE        "ps2smb"
 #define SMB2TEST_USER         "1"
