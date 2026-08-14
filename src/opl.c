e933ab5bcc9874ca5cae0cb5a70a31b6c84280e1
峰哥 <362053534@qq.com>
2026-08-15 03:51:02 +0800

    启动OPL时，将APA SMB APPS改为后台初始化

--- src/opl.c
+++ src/opl.c
@@ -1884,26 +1884,15 @@ void applyConfig(int themeID, int langID, int skipDeviceRefresh)
 
     guiUpdateScreenScale();
 
-    // 刚启动OPL时，如果不用多线程，就需要显示一帧启动画面，再进行初始化
+    // 刚启动OPL时，先显示一帧启动画面，再提交后台初始化
     if (firstOpenOPL)
         guiIntroFrame();
 
     // Check if we should refresh device support as well.
     if (skipDeviceRefresh == 0) {
-        if (firstOpenOPL) { // 第一次启动，用单线程加载所有设备，防止无限转圈
-            initAllSupport(0);
-
-            for (int i = 0; i < MODE_COUNT; i++) {
-                if (list_support[i].support == NULL)
-                    continue;
-
-                moduleUpdateMenuInternal(&list_support[i], changed, langChanged);
-            }
-        } else {
-            changed_backLoad = changed;
-            langChanged_backLoad = langChanged;
-            ioPutRequest(IO_CUSTOM_SIMPLEACTION, &loadSupportsBackground);
-        }
+        changed_backLoad = changed;
+        langChanged_backLoad = langChanged;
+        ioPutRequest(IO_CUSTOM_SIMPLEACTION, &loadSupportsBackground);
     } else {
         if (changed) {
             for (int i = 0; i < MODE_COUNT; i++) {
@@ -2550,12 +2539,9 @@ static void init(void)
         applyConfig(-1, -1, 0);
     }
 
-    // 第一次启动时，用单线程初始化
+    // 第一次启动的初始化已交给IO后台线程处理
     if (firstOpenOPL) {
-        deferredAudioInit();
-        deferredInit();
         firstOpenOPL = 0;
-        theardInitDone = 1;
     }
 }
 //int defaultSupportInitDone(void)
